ba4bba55b02b13035dc3a3e5639fb2e713e30069
KamilKKamil <k.kaczmarek@dotandspot.pl>
2023-06-20 14:53:16 +0200

    vendor deleted

--- web/includes/connection.php
+++ web/includes/connection.php
@@ -5,7 +5,6 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . '../..');
 $dotenv->load();
 
-
 $conn = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
 define("CONNECTION", $conn);
 if (!$conn) {
